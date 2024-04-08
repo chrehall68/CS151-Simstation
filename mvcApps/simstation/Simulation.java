5aedbd68426057ef236f30a3397594aa3443dcbd
Hayounl061 <hayounl061@gmail.com>
2024-04-08 16:26:44 -0700

    update simulation panel

--- mvcApps/simstation/Simulation.java
+++ mvcApps/simstation/Simulation.java
@@ -1,12 +1,30 @@
 package simstation;
 
-import mvc.Model;
+import java.util.*;
+import mvc.*;
 
 import java.util.Collection;
 
 public abstract class Simulation extends Model {
-    private int clock = 0;
     private Collection<Agent> agents;
+    transient private Timer timer; // timers aren't serializable
+    private int clock;
+
+    private void startTimer() {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new ClockUpdater(), 1000, 1000);
+    }
+
+    private void stopTimer() {
+        timer.cancel();
+        timer.purge();
+    }
+
+    private class ClockUpdater extends TimerTask {
+        public void run() {
+            clock++;
+        }
+    }
 
     public void start(){
         for(Agent a: agents){
--- mvcApps/simstation/SimulationPanel.java
+++ mvcApps/simstation/SimulationPanel.java
@@ -1,16 +1,36 @@
 package simstation;
 
-import mvc.AppFactory;
-import mvc.AppPanel;
+import mvc.*;
+
+import javax.swing.*;
 
 public class SimulationPanel extends AppPanel {
+    private JButton start;
+    private JButton suspend;
+    private JButton resume;
+    private JButton stop;
+    private JButton stats;
     public SimulationPanel(AppFactory factory) {
+
         super(factory);
-    }
-    public static void main(String[] args) {
-        SimStationFactory factory = new SimStationFactory();
-        SimulationPanel panel = new SimulationPanel(factory);
-        panel.display();
+        start = new JButton("Start");
+        suspend = new JButton("Suspend");
+        resume = new JButton("Resume");
+        stop = new JButton("Stop");
+        stats = new JButton("Stats");
+
+        start.addActionListener(SimulationPanel.this);
+        suspend.addActionListener(SimulationPanel.this);
+        resume.addActionListener(SimulationPanel.this);
+        stop.addActionListener(SimulationPanel.this);
+        stats.addActionListener(SimulationPanel.this);
+
+        controls.add(start);
+        controls.add(suspend);
+        controls.add(resume);
+        controls.add(stop);
+        controls.add(stats);
+
     }
 
 }
